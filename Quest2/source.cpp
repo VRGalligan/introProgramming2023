4416c6e99ec48af3a0ba63cafd2ac5d0cd8e69e5
Your Name <you@example.com>
2023-01-30 19:31:53 -0500

    push

---
+++ Quest2/source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+    system ("cls"); 
+
+    cout<< " Red Team" << endl;
+    cout<< "          " << endl;
+    cout << " Weapon Control: 40% " << endl;
+    cout << " Captures: 2" << endl;
+    cout << " Ownages: 0" << endl;
+    cout << "Efficiency: 50%" << endl;
+
+    system("pause");
+
+} 
