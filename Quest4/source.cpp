1e6ed393fd93b4c75ce2f282cd9bb1c88f6476b5
Your Name <you@example.com>
2023-03-01 00:01:26 -0500

    push

--- Quest4/source.cpp
+++ Quest4/source.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 
 using namespace std;
+
+int main(){
+
+    cout << "--- MAIN MENU ---" << endl;
+    cout << "1. Change Actions " << endl;
+    cout << "2. Button Layout " << endl;
+    cout << "3. Keybindings " << endl;
+    cout << "4. OnEvent Scripts " << endl;
+    cout << "5. Main Menu Bar " << endl;
+    cout << "6. Misc Options " << endl;
+    cout << "7. Unlock Dragging " << endl;
+    cout << "8. Show Button IDs " << endl;
+    cout << "9. Show Action IDs " << endl;
+    cout << "10. COPY " << endl;
+    cout << "11. PASTE " << endl;
+    cout << "                    " << endl;
+    cout << "Select a Number:    " << endl;
+
+   
+    return 0;
+
+}
